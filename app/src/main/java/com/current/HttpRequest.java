8827c55de8291435226d7a2f18a4a2d52f6b8821
decantr <theeasterlywood@gmail.com>
2018-01-09 19:59:21 +0000

    work on http request

---
+++ app/src/main/java/com/current/HttpRequest.java
@@ -0,0 +1,119 @@
+package com.current;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by decanter on 09/01/18.
+ */
+
+public class HttpRequest extends AsyncTask<String,Void, Void>
+{
+    private String returnEntry;
+    private boolean finished;
+
+
+    public void sendPostRequest (String where) {
+        URL loc = null;
+        HttpURLConnection conn = null;
+        InputStreamReader is;
+        BufferedReader in;
+
+        try {
+            loc = new URL(where);
+        }
+        catch (MalformedURLException ex) {
+            return;
+        }
+
+        try {
+            conn = (HttpURLConnection)loc.openConnection();
+            is = new InputStreamReader (conn.getInputStream(), "UTF-8");
+            in = new BufferedReader (is);
+
+            readResponse (in);
+        }
+        catch (IOException ex) {
+
+        }
+        finally {
+            conn.disconnect();
+        }
+
+    }
+
+    public String getReturnEntry() {
+        if (!finished) {
+            return "Hold tight!";
+        }
+
+        return returnEntry;
+    }
+
+    public void readResponse(BufferedReader in) {
+        String tmp = "";
+        StringBuffer response = new StringBuffer();
+
+        do {
+            try {
+                tmp = in.readLine();
+            }
+            catch (IOException ex) {
+
+            }
+
+            if (tmp != null) {
+                response.append(tmp);
+            }
+        } while (tmp != null);
+
+        returnEntry = response.toString();
+    }
+
+    @Override
+    protected void onPostExecute (Void result) {
+        finished = true;
+
+        Log.d("Output", returnEntry);
+
+    }
+    @Override
+    protected Void doInBackground(String... params) {
+        finished = false;
+        sendPostRequest (params[0]);
+        return null;
+    }
+
+    public JSONObject getResultAsJSON() {
+        JSONObject jarr = null;
+
+        if (!finished) return null;
+
+        try {
+            jarr = new JSONObject(returnEntry);
+        }
+        catch (JSONException ex) {
+            Log.d ("Output", "Error is " + ex.getMessage());
+        }
+        return jarr;
+    }
+}
--- app/src/main/java/com/current/MainActivity.java
+++ app/src/main/java/com/current/MainActivity.java
@@ -3,14 +3,23 @@ package com.current;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.NodeList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnPref;
-    Button btnHome;
-    Button btnFeed;
+    Button btnPref, btnHome, btnFeed;
+    HttpRequest task;
+    TextView txtOut;
+    JSONObject jarr;
+//    JSONArray jarr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +28,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnPref = (Button) this.findViewById(R.id.btnPref);
         btnPref.setOnClickListener(this);
+        btnFeed = this.findViewById(R.id.btnFeed);
+        btnFeed.setOnClickListener(this);
+        btnHome = this.findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(this);
+        txtOut = this.findViewById(R.id.txtOut);
     }
 
     @Override
     public void onClick(View v) {
         if (v == btnPref) opnPref();
-        else if (v == btnFeed) opnFeed();
+//        else if (v == btnFeed) opnFeed();
+//        else if (v == btnHome) req(v);
+        else req(v);
     }
 
     public void opnPref() {
@@ -35,5 +51,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void opnFeed() {
         Intent in = new Intent(this, null);
         startActivity(in);
+    }
+
+    public void req(View v) {
+        String str;
+        NodeList n;
+
+        if (task == null) {
+            task = new HttpRequest();
+            task.execute("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=088fb1a3c9e3440db5b65f2c48c3f705");
+        }
+
+        if (v == btnHome) {
+            str = task.getReturnEntry();
+
+            txtOut.setText(str);
+        }
+        else if (v == btnFeed) {
+            JSONObject tmp;
+            try {
+                jarr = task.getResultAsJSON();
+
+                if (jarr == null) {
+                    txtOut.setText ("Still loading JSON");
+                    return;
+                }
+
+                str = "";
+
+                for (int i = 0; i < jarr.length(); i++) {
+                    tmp = jarr.getJSONObject(i);
+                    str += tmp.getString ("name") + "\n";
+
+                }
+                txtOut.setText(str);
+                txtOut.setMovementMethod(new ScrollingMovementMethod());
+
+
+            }
+            catch (JSONException ex) {
+                txtOut.setText("Some horror: " + ex.getMessage());
+            }
+
+        }
     }
 }
