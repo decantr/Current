5c5f366daf275822b1fd9bb095d1860e7f07d693
decantr <theeasterlywood@gmail.com>
2018-01-14 23:05:50 +0000

    hacked callbacks

--- app/src/main/java/com/current/MainActivity.java
+++ app/src/main/java/com/current/MainActivity.java
@@ -101,4 +101,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "URL not loaded yet!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public class HttpRequest extends AsyncTask<String,Void, Void> {
+        private String re;
+        private boolean fi;
+
+        private void readResponse(BufferedReader in) {
+            String t = "";
+            StringBuilder r = new StringBuilder();
+
+            do {
+                try {
+                    t = in.readLine();
+                } catch (IOException ignored) {
+                }
+
+                if (t != null) r.append(t);
+            } while (t != null);
+
+            re = r.toString();
+        }
+
+        public void sendPostRequest(String w) {
+            URL l;
+            HttpURLConnection c = null;
+            InputStreamReader is;
+            BufferedReader in;
+
+            try {
+                l = new URL(w);
+            } catch (MalformedURLException e) {
+                return;
+            }
+
+            try {
+                c = (HttpURLConnection) l.openConnection();
+                is = new InputStreamReader(c.getInputStream(), "UTF-8");
+                in = new BufferedReader(is);
+                readResponse(in);
+            } catch (IOException ignored) {
+            } finally {
+                c.disconnect();
+            }
+
+        }
+        
+        JSONArray getResultAsJSON() throws JSONException {
+            return fi ? new JSONObject(re).getJSONArray("articles") : null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            fi = true;
+            Log.d("Output", re);
+            req();
+        }
+
+        @Override
+        protected Void doInBackground(String... p) {
+            fi = false;
+            sendPostRequest(p[0]);
+            return null;
+        }
+    }
 }
