a67ac9d73c337f306894a7e099f9b91cbeb8b64e
decantr <theeasterlywood@gmail.com>
2018-01-15 22:27:09 +0000

    add article method

--- app/src/main/java/com/current/DBUtil.java
+++ app/src/main/java/com/current/DBUtil.java
@@ -1,13 +1,11 @@
 package com.current;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
-import org.json.JSONObject;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class DBUtil {
@@ -30,7 +28,7 @@ public class DBUtil {
 
         try {
             cur = main.query(DBHandler.getName(),
-                    new String[]{"source_name, title text, description, description, image"},
+                    new String[]{"source_name, title, description, url, image"},
                     null, null,
                     null, null, null);
         }
@@ -45,5 +43,15 @@ public class DBUtil {
         }
 
         return articles;
+    }
+    
+    void addArticle(String n, String t, String d, String u, String i){
+        ContentValues a = new ContentValues();
+
+        String[] h = {"source_name", "title", "description", "url", "image"};
+        String[] c = {n, t, d, u, i};
+
+        for (int j = 0; j < 5; j++)
+            a.put(h[j], c[j]);
     }
 }
